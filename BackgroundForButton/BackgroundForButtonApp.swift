ed865543a145c661009d99d23dd03b64ca61fcf7
Прокопьев Сергей Александрович <sprokopev@naumen.ru>
2023-10-10 20:59:45 +0500

    init

---
+++ BackgroundForButton/BackgroundForButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundForButtonApp.swift
+//  BackgroundForButton
+//
+//  Created by Сергей Прокопьев on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundForButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackgroundForButton/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  BackgroundForButton
+//
+//  Created by Сергей Прокопьев on 10.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State private var isTapped: Bool = false
+
+    var body: some View {
+        Button {
+            isTapped.toggle()
+        } label: {
+            HStack(spacing: -6) {
+                if isTapped {
+                    Image(systemName: "play.fill")
+                        .transition(.scale.combined(with: .opacity))
+                }
+
+                Image(systemName: "play.fill")
+
+                if !isTapped {
+                    Image(systemName: "play.fill")
+                        .transition(.scale.combined(with: .opacity))
+                }
+            }
+            .font(.system(size: 40))
+            .foregroundColor(.cyan)
+            .animation(isTapped ? .spring(dampingFraction: 0.6) : nil, value: isTapped)
+            .onChange(of: isTapped) {
+                if !isTapped {
+                    isTapped.toggle()
+                }
+            }
+        }
+        .buttonStyle(CircleBackgroundStyle())
+    }
+}
+
+struct CircleBackgroundStyle: ButtonStyle {
+    @State private var isAnimating = false
+
+    private let duration = 0.22
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(.all, 36)
+            .background(isAnimating ? .gray.opacity(0.45) : .clear)
+            .containerShape(Circle())
+            .scaleEffect(isAnimating ? 0.86 : 1)
+            .animation(.easeInOut(duration: duration), value: isAnimating)
+            .onChange(of: configuration.isPressed) {
+                isAnimating = true
+                DispatchQueue.main.asyncAfter(deadline: .now() +  duration) {
+                    isAnimating = false
+                }
+            }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
